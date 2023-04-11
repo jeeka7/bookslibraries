f3da0022c7d734e3fab3757dc0d353eb4dfc2c1e
Dean Hunter <jeeka7@gmail.com>
2023-04-11 23:12:51 +0530

    Update rdmsgsheets.py

--- rdmsgsheets.py
+++ rdmsgsheets.py
@@ -1,51 +1,19 @@
-import streamlit as st
-import pandas as pd
+import sqlite3
 
-# Load the Google Sheets database into a pandas DataFrame
-url = 'https://docs.google.com/spreadsheets/d/1xW-KOWkyjw5um3Tq6gYsvo00c1Q0pNDWJyr5K7JZPQU/edit#gid=0'
-df = pd.read_csv(url)
+# Connect to SQLite database
+conn = sqlite3.connect('library.db')
+cursor = conn.cursor()
 
-# Set the Library ID column as the index
-df.set_index('Library ID (Primary Key)', inplace=True)
+# Create the library table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS library (
+    library_id INTEGER PRIMARY KEY,
+    library_name TEXT,
+    due_date TEXT,
+    total_fine REAL
+)
+''')
 
-# Streamlit app
-def app():
-    st.title("Library Database")
-    st.write("Welcome to the Library Database!")
-
-    # Show the data in a table
-    st.write("Library Data:")
-    st.dataframe(df)
-
-    # Add new record
-    st.subheader("Add New Record")
-    library_id = st.text_input("Library ID")
-    library_name = st.text_input("Library Name")
-    due_date = st.text_input("Due Date")
-    total_fine = st.text_input("Total Fine")
-    add_button = st.button("Add Record")
-    if add_button:
-        # Append new record to the DataFrame
-        new_record = pd.Series([library_name, due_date, total_fine],
-                              index=['Library Name', 'Due Date', 'Total Fine'])
-        df = df.append(new_record, name=library_id)
-        # Update the Google Sheets database
-        df.to_csv(url, index_label='Library ID (Primary Key)')
-        st.success("Record added successfully!")
-
-    # Delete record
-    st.subheader("Delete Record")
-    delete_id = st.text_input("Library ID to Delete")
-    delete_button = st.button("Delete Record")
-    if delete_button:
-        if delete_id in df.index:
-            # Remove record from the DataFrame
-            df = df.drop(delete_id)
-            # Update the Google Sheets database
-            df.to_csv(url, index_label='Library ID (Primary Key)')
-            st.success("Record deleted successfully!")
-        else:
-            st.warning("Library ID not found!")
-
-if __name__ == '__main__':
-    app()
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
