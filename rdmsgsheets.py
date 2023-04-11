f49dd7be0476fd6100f28aaf621c85358e5b7c9d
Dean Hunter <jeeka7@gmail.com>
2023-04-12 01:28:31 +0530

    Update rdmsgsheets.py

--- rdmsgsheets.py
+++ rdmsgsheets.py
@@ -10,7 +10,7 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets[gsheets_url])
+df = load_data(st.secrets["gsheets_url"])
 
 # Print results.
 for row in df.itertuples():
